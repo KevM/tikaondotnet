613c0ab081741e29f724ce44d3d1653ec643c36a
Kevin Miller <scoundrel@gmail.com>
2017-04-23 15:48:48 -0500

    Add sad tests which should fail if fixed by Tika (#98)
    * Confirmed #81 is a Tika bug
    
    - Added sad files which should hopefully fail if these are fixed in the future
    
    * Fix broken test and file naming

---
+++ src/TikaOnDotNet.Tests/sad-files/sad_text_extraction.cs
@@ -0,0 +1,39 @@
+using System; 
+using FluentAssertions;
+using NUnit.Framework;
+using TikaOnDotNet.TextExtraction;
+
+namespace TikaOnDotNet.Tests
+{
+    [TestFixture]
+    public class sad_text_extraction
+    {
+        private TextExtractor _cut;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _cut = new TextExtractor();
+        }
+
+        [Test]
+        public void issue_81_file_1()
+        {
+            var filePath = $"sad-files/EI-73-1018-2_5632837.doc";
+
+            Action act = () => _cut.Extract(filePath);
+
+            act.ShouldThrow<TextExtractionException>();
+        }
+
+        [Test]
+        public void issue_81_file_2()
+        {
+            var filePath = $"sad-files/EI-73-1027-3_5632849.doc";
+
+            Action act = () => _cut.Extract(filePath);
+
+            act.ShouldThrow<TextExtractionException>();
+        }
+    }
+}
